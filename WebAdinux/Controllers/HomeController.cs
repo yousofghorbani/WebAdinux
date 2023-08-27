e4de13db48ba5e63552d9fc594d0536d34b2c7af
Your Name <you@example.com>
2023-08-27 15:53:07 +0330

    fix

--- WebAdinux/Controllers/HomeController.cs
+++ WebAdinux/Controllers/HomeController.cs
@@ -17,6 +17,46 @@ namespace WebAdinux.Controllers
         {
             return View();
         }
+        [Route("/Adilyze")]
+        public IActionResult Adilyze()
+        {
+            return View();
+        }
+        [Route("/Adicamp")]
+        public IActionResult Adicamp()
+        {
+            return View();
+        }
+        [Route("/Adibox")]
+        public IActionResult Adibox()
+        {
+            return View();
+        }
+        [Route("/Adishop")]
+        public IActionResult Adishop()
+        {
+            return View();
+        }
+        [Route("/Adishow")]
+        public IActionResult Adishow()
+        {
+            return View();
+        }
+        [Route("/Adimag")]
+        public IActionResult Adimag()
+        {
+            return View();
+        }
+        [Route("/Adiroom")]
+        public IActionResult Adiroom()
+        {
+            return View();
+        }
+        [Route("/Adimatch")]
+        public IActionResult Adimactch()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
