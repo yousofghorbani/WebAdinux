294b5989bc7553b7630035c9a16cc6c5e253f1dc
Your Name <you@example.com>
2023-08-30 10:42:55 +0330

    fix

--- WebAdinux/Program.cs
+++ WebAdinux/Program.cs
@@ -1,8 +1,10 @@
+using AspNetCore.ReCaptcha;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddReCaptcha(builder.Configuration.GetSection("ReCaptcha"));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
