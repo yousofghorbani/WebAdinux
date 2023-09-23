a63671d5d92d1d0bc8d0c7eb1c24b52f5f24f439
Your Name <you@example.com>
2023-09-23 11:51:12 +0330

    fix

---
+++ WebAdinux/ViewComponents/HeaderComponent/HeaderComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAdinux.Core.Interfaces;
+
+namespace WebAdinux.ViewComponents.HeaderComponent
+{
+    public class HeaderComponent: ViewComponent
+    {
+        private readonly ISiteHeader _header;
+
+        public HeaderComponent(ISiteHeader header)
+        {
+            _header = header;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var res = await _header.Filter(null);
+            return await Task.FromResult((IViewComponentResult)View("MyHeaderView", res));
+        }
+    }
+}
